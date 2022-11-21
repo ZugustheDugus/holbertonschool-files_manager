a08a8f226add37306d368f27250390ef5d0587fe
ZugustheDugus <h1cooper2019@protonmail.com>
2022-11-21 14:10:50 -0700

    task 2

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const router = require('./routes/index');
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
